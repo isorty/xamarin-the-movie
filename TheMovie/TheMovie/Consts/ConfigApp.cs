e1c8914259f3c0063c8b58060751c8d014f45d7d
Павел Горбунов <ssorty@ya.ru>
2020-12-18 06:12:59 +0400

    Refactored

--- TheMovie/TheMovie/Consts/ConfigApp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mobile.Consts
-{
-    public static class ConfigApp
-    {
-        public const string ProjectName = "The Movie";        
-        public const string WebUrl = "themoviedb.org";
-        public const string MovieSharedPath = "/movie/";                
-    }
-}
